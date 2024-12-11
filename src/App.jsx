f8b416a7dc1e0a33066c2bd46d708606c39d6e0e
Minoru-Abe <tomominoru.abe@gmail.com>
2024-12-11 22:21:47 +0900

    nested routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import "./App.css";
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
 import { Page2 } from "./Page2";
+import { Page1DetailA } from "./Page1DetailA";
 
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { Page1DetailB } from "./Page1DetailB";
 
 export const App = () => {
   return (
@@ -18,7 +20,11 @@ export const App = () => {
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page1">
+          <Route index element={<Page1 />} />
+          <Route path="detailA" element={<Page1DetailA />} />
+          <Route path="detailB" element={<Page1DetailB />} />
+        </Route>
         <Route path="/page2" element={<Page2 />} />
       </Routes>
     </BrowserRouter>
--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,13 @@
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+
 export const Page1 = () => {
     return (
         <div>
             <h1>Page1</h1>
+            <Link to="/page1/detailA">detailA</Link>
+            <br />
+            <Link to="/page1/detailB">detailB</Link>
+            <br />
         </div>
     )
 };
