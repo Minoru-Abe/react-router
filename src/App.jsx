95fafa504999eaf90a6e360da3d51c4059aa81b4
Minoru-Abe <tomominoru.abe@gmail.com>
2024-12-11 21:54:22 +0900

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import { Home } from './Home';
+import "./App.css";
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
+
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 
 export const App = () => {
   return (
-    <Home />
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+        <br />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
