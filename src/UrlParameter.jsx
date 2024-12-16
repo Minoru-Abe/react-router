61ecf15b4195180fa90554fa7cd9fdef9a24fd7e
Minoru-Abe <tomominoru.abe@gmail.com>
2024-12-16 23:03:24 +0900

    added useLocation and URLSearchParams

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 export const UrlParameter = () => {
     const { id } = useParams();
+    const { search } = useLocation();
+    const query = new URLSearchParams(search);
   return (
     <div>
       <h1>UrlParameter</h1>
       <p> ID is {id} </p>
+      <p> name is {query.get("name")}; </p>
     </div>
   );
 };
